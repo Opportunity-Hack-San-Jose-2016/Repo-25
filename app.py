a57c16077482bc39a835cab8eededbfe075d6f1b
Pooja Shroff <poshroff@paypalcorp.com>
2016-07-10 13:31:41 -0700

    Final changes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 import random
+import gspread
 
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 @app.route("/")
 def index():
@@ -10,66 +14,45 @@ def index():
    
 @app.route("/submitInfo", methods=["GET", "POST"])
 def submitFormData():
-   formData= {};
-   name = request.form["name"];
-   formData["name"]=str(name);
-   address = request.form["address"];
-   formData["address"]=str(address);
-   phone1 = request.form["phone1"];
-   formData["phone1"]=str(phone1);
-   phone2 = request.form["phone2"];
-   formData["phone2"]=str(phone2);
-   Email1 = request.form["Email1"];
-   formData["Email1"]=str(Email1);
-   Email2 = request.form["Email2"];
-   formData["Email2"]=str(Email2);
-   live = request.form["live"];
-   formData["live"]=str(live);
-   live2 = request.form["live2"];
-   formData["live2"]=str(live2);
-   work = request.form["work"];
-   formData["work"]=str(work);
-   work2 = request.form["work2"];
-   formData["work2"]=str(work2);
-   home = request.form["home"];
-   formData["home"]=str(home);
-   adult = request.form["adult"];
-   formData["adult"]=str(adult);
-   children = request.form["children"];
-   formData["children"]=str(children);
-   address2 = request.form["address2"];
-   formData["address2"]=str(address2);
-   sold = request.form["sold"];
-   formData["sold"]=str(sold);
-   lender = request.form["lender"];
-   formData["lender"]=str(lender);
-   company = request.form["company"];
-   formData["company"]=str(company);
-   phone3 = request.form["phone3"];
-   formData["phone3"]=str(phone3);
-   monthly = request.form["monthly"];
-   formData["monthly"]=str(monthly);
-   yearly = request.form["yearly"];
-   formData["yearly"]=str(yearly);
-   down = request.form["down"];
-   formData["down"]=str(down);
-   source = request.form["source"];
-   formData["source"]=str(source);
-   assets = request.form["assets"];
-   formData["assets"]=str(assets);
-   hud = request.form["hud"];
-   formData["hud"]=str(hud);
-   contact = request.form["contact"];
-   formData["contact"]=str(contact);
-   three = request.form["three"];
-   formData["three"]=str(three);
-   bmp = request.form["bmp"];
-   formData["bmp"]=str(bmp);
-   hud2 = request.form["hud2"];
-   formData["hud2"]=str(hud2);
-   assets2 = request.form["assets2"];
-   formData["assets2"]=str(assets2);
+   scope = ['https://spreadsheets.google.com/feeds']
+   credentials = ServiceAccountCredentials.from_json_keyfile_name('My Project-45323bc9a439.json', scope)
+   gc = gspread.authorize(credentials)
+   sheet1 = gc.open_by_key('1vSpb9VceQ21GzBA2DtsjaGZPy401HU2cwi3J6hxUZFw')
+   worksheet = sheet1.get_worksheet(0)
+   
+   formData= []
+   
+   formData.append(str(request.form["name"]))
+   formData.append(str(request.form["address"]))
+   formData.append(str(request.form["phone1"]))
+   formData.append(str(request.form["phone2"]))
+   formData.append(str(request.form["Email1"]));
+   formData.append(str(request.form["Email2"]));
+   formData.append(str(request.form["live"]));
+   formData.append(str(request.form["live2"]));
+   formData.append(str(request.form["work"]));
+   formData.append(str(request.form["work2"]));
+   formData.append(str(request.form["home"]));
+   formData.append(str(request.form["adult"]));
+   formData.append(str(request.form["children"]));
+   formData.append(str(request.form["address2"]));
+   formData.append(str(request.form["sold"]));
+   formData.append(str(request.form["lender"]));
+   formData.append(str(request.form["company"]));
+   formData.append(str(request.form["phone3"]));
+   formData.append(str(request.form["monthly"]));
+   formData.append(str(request.form["yearly"]));
+   formData.append(str(request.form["down"]));
+   formData.append(str(request.form["source"]));
+   formData.append(str(request.form["assets"]));
+   formData.append(str(request.form["hud"]));
+   formData.append(str(request.form["contact"]));
+   formData.append(str(request.form["three"]));
+   formData.append(str(request.form["bmp"]));
+   formData.append(str(request.form["hud2"]));
+   formData.append(str(request.form["assets2"]));
    print formData
+   worksheet.append_row(formData)
    return render_template("result.html");
 
 if __name__ == "__main__":
